180bab867425cf7a60d14127f3f64f3c2598e003
Karan0345 <karanvishwakarma0345@gmail.com>
2024-03-28 16:44:47 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Navbar from './Components/Navbar';
+import Hero from './Components/Hero';
+import Highlights from './Components/Highlights';
+
+
+const App = () => {
+  
+  return (
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+    </main>
+  )
+}
+
+export default (App);
